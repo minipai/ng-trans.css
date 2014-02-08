45c6043e2508bc76cf39894dc429ad994b69eabc
Art Pai <minipai@gmail.com>
2014-02-09 02:24:33 +0800

    switch to Stylus

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,18 @@
 var gulp    = require('gulp')
 ,   connect = require('gulp-connect')
+,   stylus  = require('gulp-stylus')
 
 gulp.task('connect', connect.server({
   root: __dirname + '/document',
   port: 1337,
   open: { browser: 'Google Chrome'},
   })
-)
+);
 
-gulp.task('default', ['connect'])
+gulp.task('stylus', function() {
+  gulp.src('./source/ng-trans.styl')
+      .pipe(stylus())
+      .pipe(gulp.dest('./'))
+});
+
+gulp.task('default', ['stylus'])
