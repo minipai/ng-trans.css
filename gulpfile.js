27ed77dedf626614186541098fedb273bfc4bb23
Art Pai <minipai@gmail.com>
2014-02-09 12:13:13 +0800

    complete gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var gulp    = require('gulp')
 ,   stylus  = require('gulp-stylus')
 ,   prefix  = require('gulp-autoprefixer');
 
+
 gulp.task('connect', connect.server({
   root: __dirname + '/document',
   port: 1337,
@@ -15,7 +16,20 @@ gulp.task('stylus', function() {
       .pipe(stylus())
       .pipe(prefix("last 2 version", "> 1%", "ie 8"))
       .pipe(gulp.dest('./'))
+      .pipe(gulp.dest('document/'))
+});
 
+gulp.task('watch', function () {
+  gulp.watch('source/*.styl', ['stylus']);
 });
 
 gulp.task('default', ['stylus'])
+
+
+gulp.task('dev', function(){
+  gulp.run('connect', 'stylus');
+
+  gulp.watch('source/*.styl' , function(ev){
+        gulp.run('stylus')
+    });
+});
