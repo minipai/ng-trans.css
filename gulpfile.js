b6a6d9571a59a369d9bdfde2f6e7c2fc88ecb6bd
Art Pai <minipai@gmail.com>
2014-02-19 08:12:22 +0800

    improve gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
 var gulp    = require('gulp')
-,   connect = require('gulp-connect')
-,   stylus  = require('gulp-stylus')
-,   prefix  = require('gulp-autoprefixer');
+var connect = require('gulp-connect')
+var stylus  = require('gulp-stylus')
+var prefix  = require('gulp-autoprefixer');
+var plumber = require('gulp-plumber');
 
+var compileStylus = function(src) {
+  gulp.src(src)
+      .pipe(plumber())
+      .pipe(stylus())
+      .pipe(prefix("last 2 version", "> 1%", "ie 8"))
+      .pipe(gulp.dest('./'))
+      .pipe(gulp.dest('document/'));
+};
 
 gulp.task('connect', connect.server({
   root: __dirname + '/document',
@@ -12,24 +21,20 @@ gulp.task('connect', connect.server({
 );
 
 gulp.task('stylus', function() {
-  gulp.src('./source/ng-trans.styl')
-      .pipe(stylus())
-      .pipe(prefix("last 2 version", "> 1%", "ie 8"))
-      .pipe(gulp.dest('./'))
-      .pipe(gulp.dest('document/'))
+  compileStylus('./source/ng-trans.styl');
 });
 
 gulp.task('watch', function () {
   gulp.watch('source/*.styl', ['stylus']);
 });
 
-gulp.task('default', ['stylus'])
+gulp.task('default', ['stylus']);
 
-
-gulp.task('dev', function(){
+gulp.task('serve', function(){
   gulp.run('connect', 'stylus');
 
-  gulp.watch('source/*.styl' , function(ev){
-        gulp.run('stylus')
-    });
+  gulp.watch('source/*.styl', function(event){
+    console.log('File '+event.path+' was '+event.type+', running tasks...');
+    compileStylus('./source/ng-trans.styl');
+  });
 });
