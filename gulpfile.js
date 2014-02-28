3775ca5d792ac3778dbea536223d53dc56f87e38
Art Pai <minipai@gmail.com>
2014-02-28 20:05:26 +0800

    Add minified css

--- gulpfile.js
+++ gulpfile.js
@@ -6,39 +6,43 @@ var stylus  = require('gulp-stylus')
 var prefix  = require('gulp-autoprefixer')
 var plumber = require('gulp-plumber')
 
-var compileStylus = function(src) {
-  gulp.src(src)
+var rename = require("gulp-rename");
+var minify = require('gulp-minify-css');
+
+
+gulp.task('stylus', function() {
+  var stream = gulp.src('./source/ng-trans.styl')
       .pipe(plumber())
       .pipe(stylus())
       .pipe(prefix("last 2 version", "> 1%", "ie 8"))
       .pipe(gulp.dest('./'))
       .pipe(gulp.dest('document/'));
-};
+  return stream
+});
+
+gulp.task('minify', ['stylus'], function() {
+  var stream = gulp.src('./ng-trans.css')
+    .pipe(rename({suffix: ".min"}))
+    .pipe(minify(opts))
+    .pipe(gulp.dest('./'))
+  return stream
+});
 
 gulp.task('connect', function() {
   connect.createServer(
-    connect.static(__dirname + '/document')
-  ).listen(8080);
+    connect.static('./document')
+  ).listen(5000);
 
-  open("http://0.0.0.0:8080", "Google Chrome");
-
+  open("http://0.0.0.0:5000", "Google Chrome");
 });
 
-gulp.task('stylus', function() {
-  compileStylus('./source/ng-trans.styl');
-});
-
-gulp.task('watch', function () {
-  gulp.watch('source/*.styl', ['stylus']);
-});
-
-gulp.task('default', ['stylus']);
-
-gulp.task('serve', function(){
-  gulp.run('connect', 'stylus');
-
-  gulp.watch('source/*.styl', function(event){
-    console.log('File '+event.path+' was '+event.type+', running tasks...');
-    compileStylus('./source/ng-trans.styl');
+gulp.task('watch', function() {
+  var watcher = gulp.watch('./source/*.styl', function(){
+    gulp.start('stylus')
+    gulp.start('minify')
   });
 });
+
+gulp.task('serve', ['connect', 'stylus', 'minify', 'watch'])
+
+gulp.task('default', ['stylus', 'minify']);
